1e3228a5feb7551c5cb2542dc0e6845220acfcc0
Riko <riko.fluchel@gmail.com>
2025-01-23 07:34:40 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+	let form = document.querySelector('form')
+
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
+		const taskValue = document.getElementById("new-task-description");
+		const priorityValue = document.getElementById("priority").value;
+		buildToDo(taskValue.value, priorityValue);
+		form.reset();
+  })  
 });
+
+
+function buildToDo(todo, priority){
+	let ul = document.createElement('ul');
+	let btn = document.createElement('button');
+	btn.addEventListener('click', handleDelete);
+	btn.textContent = ' x';
+	ul.textContent = `${todo} `;
+	if (priority === "high"){
+		ul.className = "high";
+	} else if (priority === "medium"){
+		ul.className = "medium";
+	} else if (priority === "low"){
+		ul.className = "low";
+	}
+	ul.appendChild(btn);
+	document.querySelector('h2').appendChild(ul);
+}
+
+function handleDelete(e){
+	e.target.parentNode.remove()
+}
+
